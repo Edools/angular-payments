6dd6e6e96b302b57b2d77b877284821d13e8b7ad
Vinicius Kastrup <viniciusmkm@gmail.com>
2015-07-27 17:20:19 -0300

    move elo validation to match first

--- src/cards.js
+++ src/cards.js
@@ -15,6 +15,14 @@ angular.module('angularPayments')
       cvcLength: [3],
       luhn: true
     }, {
+      type: 'elo',
+      pattern: /^(636368|438935|504175|451416|509048|509067|509049|509069|509050|509074|509068|509040|509045|509051|509046|509066|509047|509042|509052|509043|509064|509040|36297|5067|4576|4011)/,
+      format: defaultFormat,
+      inputFormat: defaultInputFormat,
+      length: [16],
+      cvcLength: [3],
+      luhn: true
+    }, {
       type: 'dinersclub',
       pattern: /^(36|38|30[0-5])/,
       format: defaultFormat,
@@ -84,14 +92,6 @@ angular.module('angularPayments')
       format: defaultFormat,
       inputFormat: defaultInputFormat,
       length: [13, 16, 19],
-      cvcLength: [3],
-      luhn: true
-    }, {
-      type: 'elo',
-      pattern: /^(636368|438935|504175|451416|509048|509067|509049|509069|509050|509074|509068|509040|509045|509051|509046|509066|509047|509042|509052|509043|509064|509040|36297|5067|4576|4011)/,
-      format: defaultFormat,
-      inputFormat: defaultInputFormat,
-      length: [16],
       cvcLength: [3],
       luhn: true
     }
